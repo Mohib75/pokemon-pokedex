8d4a16387fe0be1f67551509dba67c72deb19103
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-09-17 18:36:44 +0600

    loader added

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+	return (
+		<div className='flex justify-center animate-spin'>
+			<img className='w-40 h-40' src='/spinner.svg' alt='spinner' />
+		</div>
+	)
+}
+
+export default Spinner
